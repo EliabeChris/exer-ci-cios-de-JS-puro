e4d45dfe223337d74f4d0c7f3185a9ad54803464
Eliabe Christopher <eliabechris@hotmail.com>
2025-03-13 21:14:37 -0300

    Conclusão exercício 13

---
+++ ex13.js
@@ -0,0 +1,30 @@
+function diaDaSemana(dia) {
+    switch (dia) {
+        case 1:
+            console.log("Domingo: Fim de semana")
+            break
+        case 2:
+            console.log("Segunda-feira: Dia últil")
+            break
+        case 3:
+            console.log("Terça-Feira: Dia últil")
+            break
+        case 4:
+            console.log("Quarta-Feira: Dia últil")
+            break
+        case 5:
+            console.log("Quinta-Feira: Dia últil")
+            break
+        case 6:
+            console.log("Sexta-feira: Dia últil")
+            break
+        case 7:
+            console.log("Sábado: Fim de semana")
+            break
+            default:
+                console.log("Dia inválido!")
+        } 
+
+}
+
+diaDaSemana(1)
