bd2471a0ae1e162494fd2229847af8447c80d5b8
Eliabe Christopher <eliabechris@hotmail.com>
2025-03-14 00:18:16 -0300

    Conclusão do exercício 18

---
+++ ex18.js
@@ -0,0 +1,41 @@
+function mostratNumero(numero) {
+    switch (numero) {
+        case 0:
+            console.log("zero")
+            break
+        case 1:
+            console.log("um")
+            break
+        case 2:
+            console.log("Dois")
+            break
+        case 3:
+            console.log("Tres")
+            break
+        case 4:
+            console.log("Quatro")
+            break
+        case 5:
+            console.log("Cinco")
+            break
+        case 6:
+            console.log("Seis")
+            break
+        case 7:
+            console.log("Sete")
+            break
+        case 8:
+            console.log("Oito")
+            break
+        case 9:
+            console.log("Nove")
+            break
+        case 10:
+            console.log("Dex")
+            break
+        default:
+            console.log("Número fora do intervalo")
+
+    }
+}
+mostratNumero(0)
