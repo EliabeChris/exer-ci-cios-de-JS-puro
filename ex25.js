b0c5356785b594a9f6086ede1d7f4f819f12dc16
Eliabe Christopher <eliabechris@hotmail.com>
2025-03-16 09:04:52 -0300

    Conclusão exercício 25

---
+++ ex25.js
@@ -0,0 +1,10 @@
+function repetirNumeroEmOrdemCrescente (numero) {
+    let num = numero
+    let contador = 0
+    
+    while(contador < num ) {
+        contador++
+        console.log(`${contador}`)
+    }
+}
+repetirNumeroEmOrdemCrescente(50)
